571ca405c72bf66edaec3cd360d0aaa017f5b4f1
iffarurrahat <iffarurrahat2018@gmail.com>
2021-10-03 01:57:38 +0600

    updated the inventroy page

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Inventory from './Inventory';
 
 const Inventory = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h2>This is Inventory page</h2>
+    </div>
+  );
 };
 
 export default Inventory;
